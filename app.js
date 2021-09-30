302b1cbde1a0e57771b144beda8dbe7810182e22
Vicente Garcia <vicentegarciaayllon@gmail.com>
2021-09-30 16:57:58 +0200

    finish project

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-let pokemonContainer = document.getElementById("pokemons");
+let count = 1;
 
-pokemonContainer.innerHTML = 
+function renderImage() {
+    let pokemonContainer = document.getElementById("pokemons");
+
+    pokemonContainer.innerHTML = `<img id="pokemon-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${count}.svg" alt="pokemon image">`
+}
+
+let previousPoke = document.getElementById("previous");
+let nextPoke = document.getElementById("next");
+
+previousPoke.onclick = function() {
+    if (count > 1) {
+        count -= 1;
+        renderImage();
+    }
+}
+
+nextPoke.onclick = function() {
+    if (count < 649) {
+        count += 1;
+        renderImage();
+    }
+}
+
+renderImage();
